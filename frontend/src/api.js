c25934a9fd7cd89a8c0445ba5ae19556162c4bf7
uikeytulsidas <uikeytulsidas@gmail.com>
2025-09-19 14:51:25 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL =   "https://career-path-ai-sc0h.onrender.com/api",
- withCredentials: true, 
+const API_URL =   process.env.REACT_APP_API_URL;// Django backend
+ // withCredentials: true, 
 
 // Axios instance with token support
 const api = axios.create({
